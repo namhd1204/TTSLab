f21af68283f7f00af8f4f86b26e618c51a4d6f1d
hdnam1204 <namhd.1204@gmail.com>
2026-05-03 23:58:08 +0700

    chore: clean up stt.py by removing commented code and unnecessary imports

--- libs/stt.py
+++ libs/stt.py
@@ -5,13 +5,8 @@ load_dotenv()
 
 import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
-from datasets import load_dataset
 from rainbow_print import rprint
 
-
-
-# import os
-# os.environ["HF_TOKEN"] = "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" # Thay bằng token của bạn
 os.environ["HF_TOKEN"] = os.getenv('HF_TOKEN')
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -23,10 +18,8 @@ rprint.info('loading model...')
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, 
     dtype=torch_dtype, 
-    # torch_dtype=torch_dtype, 
     low_cpu_mem_usage=True, 
     use_safetensors=True,
-    # clean_up_tokenization_spaces=False
 )
 
 rprint.info('loading model to device...')
@@ -41,18 +34,10 @@ pipe = pipeline(
     model=model,
     tokenizer=processor.tokenizer,
     feature_extractor=processor.feature_extractor,
-    # max_new_tokens=128,
     dtype=torch_dtype,
-    # torch_dtype=torch_dtype,
     device=device,
     stride_length_s=5
 )
-
-# rprint.info('loading dataset...')
-# dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-# sample = dataset[0]["audio"]
-# # <class 'datasets.features._torchcodec.AudioDecoder'>
-# rprint.info(str(type(sample)))
 
 result = pipe('output/tiger.wav', generate_kwargs={"language": "english"})
 rprint.info("Transcription result:")
